b03a2eb16636185df0ba7dbada69ab7822450e56
Alexander Koskovich <zvnexus@outlook.com>
2020-12-01 13:43:18 -0700

    configs: Enable blurs
     * Required for the blur option in Settings to work,
       if the user wants to turn off blur for performance
       reasons they can toggle.
    
    Change-Id: Id21aaba8187d211892da689a3cb622b801510343

--- config/common.mk
+++ config/common.mk
@@ -76,3 +76,8 @@ SQUISHER_SCRIPT := vendor/banana/tools/squisher
 
 # Disable vendor restrictions
 PRODUCT_RESTRICT_VENDOR_FILES := false
+
+# Blurs
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.sf.blurs_are_expensive=1 \
+    ro.surface_flinger.supports_background_blur=1
