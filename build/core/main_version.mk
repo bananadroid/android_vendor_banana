c84daa8c8a556f59289233aaf857a9c4f04f7d25
Danny Lin <danny@kdrag0n.dev>
2021-04-15 22:36:39 -0700

    Only use stock build fingerprint for Google Play Services
    SafetyNet's CTS profile attestation checks whether Build.FINGERPRINT
    matches that of the device's stock OS, which has passed CTS testing.
    This means that we need to spoof the fingerprint for Google Play
    Services in order to pass SafetyNet.
    
    We used to set the real system build fingerprint to the stock one, but
    Android relies on each build having a unique fingerprint in order to
    clear the correct caches and update persistent state for system changes.
    On devices that no longer receive updates from the OEM, the build
    fingerprint never changes and Android doesn't account for updates
    correctly, which causes issues when updating without wiping data.
    Only spoofing the fingerprint for Google Play Services fixes this issue.
    
    Change-Id: I368b1ca0fe985b31bcd5dc548c6f461e55923228

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -4,7 +4,8 @@ ADDITIONAL_BUILD_PROPERTIES += \
     ro.banana.version=$(BANANA_VERSION) \
     ro.banana.build.version=$(BANANA_REVISION) \
     ro.banana.build_date=$(BANANA_BUILD_DATE) \
-    ro.banana.releasetype=$(BANANA_BUILDTYPE)
+    ro.banana.releasetype=$(BANANA_BUILDTYPE) \
+    ro.build.stock_fingerprint=$(TARGET_FORCE_BUILD_FINGERPRINT)
 
 # Build fingerprint
 ifneq ($(BUILD_FINGERPRINT),)
