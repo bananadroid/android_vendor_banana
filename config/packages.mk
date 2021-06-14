f2d458498acc0f0a621f59154a3ddd2d2f40f02e
Michael Bestas <mkbestas@gmail.com>
2021-06-14 15:04:40 +0300

    config: Exclude some files from artifact path requirements
    Change-Id: Id5ef2a44c0cd3b76e3285c26826031f773b7a95f
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- config/packages.mk
+++ config/packages.mk
@@ -50,3 +50,13 @@ PRODUCT_PACKAGES += \
 	libtextclassifier_annotator_universal_model \
 	libtextclassifier_actions_suggestions_universal_model \
 	libtextclassifier_lang_id_model
+
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/bin/curl \
+    system/bin/getcap \
+    system/bin/setcap
+
+ifneq ($(TARGET_BUILD_VARIANT),user)
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/bin/procmem
+endif
