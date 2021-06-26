maldini03/vendor_banana

vendor: Allow forcing custom build fingerprint [1/2]
Change-Id: I7f5fadcaae9c789f95a0dbc5b6b912b7fb70861a

config/common.mk
@@ -1,6 +1,8 @@
 PRODUCT_BRAND ?= BananaDroid
 BANANA_BUILD := true
 
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys
+
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.clientidbase=android-google

bananadroid/android_vendor_banana

config/common.mk
@@ -1,6 +1,8 @@
 PRODUCT_BRAND ?= BananaDroid
 BANANA_BUILD := true
 
+TARGET_FORCE_BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys
+
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.clientidbase=android-google