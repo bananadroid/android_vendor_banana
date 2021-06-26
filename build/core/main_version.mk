9ade79c6d9122fc13316b35f6c2278b0ad5f92d9
Rizky Benggolo <travarilo@gmail.com>
2021-06-26 08:39:32 +0700

    Revert "build: set build fingerprint for all devices"
    This reverts commit 27180809c594cf6beeaeb206cced6fa42c086517.
    
    Change-Id: I46ff9f90baab923228e63c8ea5a72f4dbe4abd63

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -6,9 +6,3 @@ ADDITIONAL_BUILD_PROPERTIES += \
     ro.banana.build_date=$(BANANA_BUILD_DATE) \
     ro.banana.releasetype=$(BANANA_BUILDTYPE) \
     ro.build.stock_fingerprint=$(TARGET_FORCE_BUILD_FINGERPRINT)
-
-# Build fingerprint
-ifneq ($(BUILD_FINGERPRINT),)
-ADDITIONAL_BUILD_PROPERTIES += \
-    ro.build.fingerprint=$(BUILD_FINGERPRINT)
-endif
