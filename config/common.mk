032316fc39ed1ace346abb9bd0d16028c06f3203
xiaoxindada <2245062854@qq.com>
2021-07-11 14:06:20 +0000

    vendor: Revert "build: Flatten APEXs for performance"
    * As google's developer document said, flatten apex
    shouldn't, for past, for present, and for the future
    be settled to enable as default.
    
    * As @fryeviac and Shion Kaguzara tested, multiple devices
    which cannot be booted before now boots with this commit
    reverted.
    
    * Due to above reasons, this commit shall be reverted.
    
    Signed-off-by: xiaoxindada <2245062854@qq.com>
    Signed-off-by: SakuraiLH <hikarilh@foxmail.com>
    Tested-by: SakuraiLH <hikarilh@foxmail.com>
    
    This reverts commit d01ded09d10360e397152948bd4dbc20cc2cce6d.
    
    Change-Id: I366886fb599f50f0a4c532d5a1986346b1516ce2

--- config/common.mk
+++ config/common.mk
@@ -131,8 +131,6 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
     ro.sf.blurs_are_expensive=1 \
     ro.surface_flinger.supports_background_blur=1
 
-# Flatten APEXs for performance
-OVERRIDE_TARGET_FLATTEN_APEX := true
 # This needs to be specified explicitly to override ro.apex.updatable=true from
 # prebuilt vendors, as init reads /product/build.prop after /vendor/build.prop
 PRODUCT_PRODUCT_PROPERTIES += ro.apex.updatable=false
