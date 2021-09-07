70fc0ce0a537c44ecd6e97414af75808cd4d20b0
Rizky Benggolo <travarilo@gmail.com>
2021-09-07 16:02:46 +0000

    vendor: Conditionally add GApps version

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -2,6 +2,7 @@
 ADDITIONAL_BUILD_PROPERTIES += \
     ro.modversion=BananaDroid-$(BANANA_RELEASED_VERSION) \
     ro.banana.version=$(BANANA_RELEASED_VERSION) \
+    ro.banana.build.variant=$(BANANA_BUILD_VARIANT) \
     ro.banana.build.version=$(BANANA_VERSION) \
     ro.banana.build_date=$(BANANA_BUILD_DATE) \
     ro.banana.displayed.version=$(BANANA_CODENAME)-$(BANANA_VERSION) \
