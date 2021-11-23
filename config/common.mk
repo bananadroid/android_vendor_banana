403a7283ef4981be0a4e3eef30cf86fc9706d39a
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-11-23 22:48:17 +0530

    config: don't dexpreopt prebuilts
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: Ia121155ee81fa85496a678eb70f8dfacb9850636

--- config/common.mk
+++ config/common.mk
@@ -96,6 +96,8 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 # Dex preopt everything
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
+DONT_DEXPREOPT_PREBUILTS := true
+
 ifeq ($(TARGET_SUPPORTS_64_BIT_APPS), true)
 # Use 64-bit dex2oat for better dexopt time.
 PRODUCT_PROPERTY_OVERRIDES += \
