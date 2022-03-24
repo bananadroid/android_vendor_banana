3492f4e5ecafe8ea9dd2dbacaeaaa65a81e79815
Timi Rautamäki <timi.rautamaki@gmail.com>
2022-03-24 08:21:34 +0000

    build: move mka definition above brunch
    Otherwise it's not defined for brunch.
    
    Change-Id: I642e3db365fab46d4428d1b1f310304280b72efc
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -23,6 +23,8 @@ Additional BananaDroid functions:
 EOF
 }
 
+alias mka=m
+
 function mk_timer()
 {
     local start_time=$(date +"%s")
@@ -715,8 +717,6 @@ function bananarebase() {
     repo abandon tmprebase .
     cd $pwd
 }
-
-alias mka=m
 
 function cmka() {
     if [ ! -z "$1" ]; then
