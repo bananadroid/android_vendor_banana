Project-Awaken/android_vendor_awaken

vendor: Add game overlay sysconfig
Change-Id: Ie5b46b86125f1fe601bce93a24256607c7ea28d7
Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

config/common.mk
@@ -94,6 +94,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # Fonts
 include vendor/awaken/config/fonts.mk
 
+# Game Overlay
+PRODUCT_COPY_FILES += \
+    vendor/awaken/prebuilt/common/etc/sysconfig/game_overlay.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/game_overlay.xml
+
 # Gapps
 ifeq ($(USE_GAPPS),true)
 $(call inherit-product-if-exists, vendor/gms/products/gms.mk)

bananadroid/android_vendor_banana

Signed-off-by: aswin7469 <aswinas@pixysos.com>

config/common.mk
@@ -141,6 +141,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
 
+# Game Overlay
+PRODUCT_COPY_FILES += \
+    vendor/banana/prebuilt/common/etc/sysconfig/game_overlay.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/game_overlay.xml
+
 # Quick Tap
 ifeq ($(TARGET_SUPPORTS_QUICK_TAP),true)
 PRODUCT_COPY_FILES += \