cc053a9ad5b79924ef8454dc69b167ba816fb8a5
Michael Bestas <mkbestas@lineageos.org>
2022-08-30 02:39:47 +0300

    config: Remove TARGET_USES_QCOM_BSP_LEGACY
    Change-Id: I93f111285c46e1a1f811b9d8555de96474d60386

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -55,7 +55,6 @@ SOONG_CONFIG_bananaQcomVars += \
     supports_extended_compress_format \
     no_camera_smooth_apis \
     uses_pre_uplink_features_netmgrd \
-    uses_qcom_bsp_legacy \
     uses_qti_camera_device
 
 # Only create display_headers_namespace var if dealing with UM platforms to avoid breaking build for all other platforms
@@ -77,7 +76,6 @@ SOONG_CONFIG_bananaQcomVars_legacy_hw_disk_encryption := $(TARGET_LEGACY_HW_DISK
 SOONG_CONFIG_bananaQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_bananaQcomVars_no_camera_smooth_apis := $(TARGET_HAS_NO_CAMERA_SMOOTH_APIS)
 SOONG_CONFIG_bananaQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
-SOONG_CONFIG_bananaQcomVars_uses_qcom_bsp_legacy := $(TARGET_USES_QCOM_BSP_LEGACY)
 SOONG_CONFIG_bananaQcomVars_uses_qti_camera_device := $(TARGET_USES_QTI_CAMERA_DEVICE)
 
 # Set default values
