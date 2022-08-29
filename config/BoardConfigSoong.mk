22986847b262b9665c0934df900936d936bc9560
Michael Bestas <mkbestas@lineageos.org>
2022-08-30 02:43:52 +0300

    config: Remove TARGET_HAS_LEGACY_CAMERA_HAL1
    Change-Id: I6f4cbe41f6df3ceb6b4058d5b6e77c02e1899809

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -33,7 +33,6 @@ SOONG_CONFIG_bananaGlobalVars += \
     camera_needs_client_info_lib \
     target_alternative_futex_waiters \
     disable_postrender_cleanup \
-    has_legacy_camera_hal1 \
     has_memfd_backport \
     ignores_ftp_pptp_conntrack_failure \
     needs_camera_boottime \
@@ -66,7 +65,6 @@ endif
 # Soong bool variables
 SOONG_CONFIG_bananaGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
 SOONG_CONFIG_bananaGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
-SOONG_CONFIG_bananaGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA_HAL1)
 SOONG_CONFIG_bananaGlobalVars_has_memfd_backport := $(TARGET_HAS_MEMFD_BACKPORT)
 SOONG_CONFIG_bananaGlobalVars_ignores_ftp_pptp_conntrack_failure := $(TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE)
 SOONG_CONFIG_bananaGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
