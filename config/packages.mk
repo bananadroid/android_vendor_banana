afcf7c9a89638d9464da765ce6c63a64c75d43c4
Nolen Johnson <johnsonnolen@gmail.com>
2022-10-03 21:16:56 -0400

    config: Exclude ntfs-3g from artifact path requirement
    Change-Id: I6253ac0b326f86ba7fd78f6babb9ee2242ba9133

--- config/packages.mk
+++ config/packages.mk
@@ -39,6 +39,13 @@ PRODUCT_PACKAGES += \
     vim \
     zip
 
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/bin/fsck.ntfs \
+    system/bin/mkfs.ntfs \
+    system/bin/mount.ntfs \
+    system/%/libfuse-lite.so \
+    system/%/libntfs-3g.so
+
 # Openssh
 PRODUCT_PACKAGES += \
     scp \
