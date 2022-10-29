1e6c0f9759408865ae18a345342e95925b5410fe
Rizky Benggolo <travarilo@gmail.com>
2022-10-29 23:58:52 +0700

    Revert "vendor: Spoof build fingerprint for Google Play Services [2/2]"
    This reverts commit 12f34efff50ec348d97bb043028832ff9d8e3b9d.
    
    Revert "props: walleye as default GMS override prop"
    
    This reverts commit 6295879fbe862420245090a3a18251d2377ef1bb.

--- config/prop.mk
+++ config/prop.mk
@@ -89,10 +89,3 @@ PRODUCT_SYSTEM_PROPERTIES += \
 # SystemUI
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     dalvik.vm.systemuicompilerfilter=speed
-
-# Spoof fingerprint for Google Play Services and SafetyNet
-ifeq ($(PRODUCT_OVERRIDE_GMS_FINGERPRINT),)
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.build.gms_fingerprint=google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys
-else
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.build.gms_fingerprint=$(PRODUCT_OVERRIDE_GMS_FINGERPRINT)
-endif
