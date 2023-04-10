7360dfce3ad2106106629539418eff97eb0efa4c
Rizky Benggolo <travarilo@gmail.com>
2023-04-10 13:46:19 +0200

    config: Don't ship lineage compatibility matrix by default

--- config/common.mk
+++ config/common.mk
@@ -147,6 +147,4 @@ PRODUCT_PACKAGES += \
 
 $(call inherit-product, vendor/banana/config/telephony.mk)
 
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/banana/config/device_framework_matrix.xml
-
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
