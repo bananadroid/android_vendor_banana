6f94c33aad7097b9fd544a5fd0beabadfdc21b57
NurKeinNeid <mralexman3000@gmail.com>
2023-04-16 19:21:23 +0530

    vendor: Add Google Face Unlock support
    * apk imported from husky
    
    Change-Id: I1205c2cb9571a0fc92718fc03e1b0e6875aff026
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -137,6 +137,9 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+else
+PRODUCT_PACKAGES += \
+    SettingsGoogleFutureFaceEnroll
 endif
 
 # Game Overlay
