4c4ed86ba9058bc87af0a0c7fda968efb99fcc88
Shivan Singh <shivan.0972@gmail.com>
2023-04-23 07:48:04 +0800

    config: Import Common GMS properties
    * taken from pixelify
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- config/prop.mk
+++ config/prop.mk
@@ -25,6 +25,25 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
 endif
 
+# Common GMS properties
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.rcs.supported=1 \
+    ro.opa.eligible_device=true \
+    ro.com.google.ime.theme_id=5 \
+    ro.com.google.ime.bs_theme=true \
+    ro.com.google.ime.system_lm_dir=/product/usr/share/ime/google/d3_lms \
+    ro.product.locale=en-US \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.product.needs_model_edit=false \
+    ro.support_one_handed_mode=true \
+    persist.sys.storage_picker_enabled=true \
+    debug.photos.p_editr.eraser=1 \
+    debug.photos.force_pixel_eol=1 \
+    debug.photos.eraser_camo=1 \
+    debug.photos.eraser_suggestion=1 \
+    ro.support_hide_display_cutout=true
+
 # Disable extra StrictMode features on all non-engineering builds
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.strictmode.disable=true
 endif
