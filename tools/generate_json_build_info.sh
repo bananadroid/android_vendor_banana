a1e36b2a6d84da684a6584e7c562e099baddd0ef
Rizky Benggolo <travarilo@gmail.com>
2023-05-07 17:48:41 +0000

    tools: Improve build info generator
    Change-Id: I0c65eefff90d248a4c415795c101a5c1e9d311a2

--- tools/generate_json_build_info.sh
+++ tools/generate_json_build_info.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
+output=$file_path.json
 if [ "$1" ]
 then
   file_path=$1
   file_name=$(basename "$file_path")
   DEVICE=$(echo $TARGET_PRODUCT | sed 's/banana_//g')
   if [ -f $file_path ]; then
-    device_name=$(echo $file_name | cut -d'-' -f4)
+    device_name=$(grep ro\.banana\.device\= ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
     file_size=$(stat -c%s $file_path)
     md5=$(cat "$file_path.md5sum" | cut -d' ' -f1)
     datetime=$(grep ro\.build\.date\.utc ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
     id=$(sha256sum $file_path | awk '{ print $1 }');
     maintainer_name=$(grep ro\.banana\.maintainer\= ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
-    echo -e "{\n   \"datetime\": $datetime,\n   \"filehash\": \"$md5\",\n   \"filename\": \"$file_name\",\n   \"id\": \"$id\",\n   \"size\": $file_size,\n   \"maintainer\": $maintainer_name,\n}" > $file_path.json
+    echo -e "{\n   \"error\": "false",\n   \"version\": \"13\",\n   \"url\": \"https://sourceforge.net/projects/bananadroid/files/$device_name/$file_name\",\n   \"github_releases_url\": \"https://sourceforge.net/projects/bananadroid/files/$device_name/$file_name\",\n   \"datetime\": $datetime,\n   \"filehash\": \"$md5\",\n   \"filename\": \"$file_name\",\n   \"id\": \"$id\",\n   \"size\": $file_size,\n   \"maintainers\": \"name\": \"$maintainer_name\",\n   \"donate_url\": \"https://paypal.me/travarilo\",\n   \"website_url\": \"https://bananadroid.com\",\n   \"forum_url\": \"https://t.me/bananadroid\"\n}" > $output
     rm -rf "./$DEVICE.json"
-    mv "${file_path}.json" "./$DEVICE.json"
+    cp $output "./$DEVICE.json"
   fi
 fi
+cat $output
+echo ""
+
