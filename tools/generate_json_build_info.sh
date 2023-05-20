81bf3e3efc895b4f7aeedcfb4d616746e5663671
Rizky Benggolo <travarilo@gmail.com>
2023-05-20 22:44:51 +0700

    tools: Improve build info generator
    Change-Id: Ibe14a7ccd9620c6b0bfd2b0915b586ee95199a31

--- tools/generate_json_build_info.sh
+++ tools/generate_json_build_info.sh
@@ -12,7 +12,7 @@ then
     datetime=$(grep ro\.build\.date\.utc ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
     id=$(sha256sum $file_path | awk '{ print $1 }');
     maintainer_name=$(grep ro\.banana\.maintainer\= ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
-    echo -e "{\n   \"error\": "false",\n   \"version\": \"13\",\n   \"url\": \"https://sourceforge.net/projects/bananadroid/files/$device_name/$file_name\",\n   \"github_releases_url\": \"https://sourceforge.net/projects/bananadroid/files/$device_name/$file_name\",\n   \"datetime\": $datetime,\n   \"filehash\": \"$md5\",\n   \"filename\": \"$file_name\",\n   \"id\": \"$id\",\n   \"size\": $file_size,\n   \"maintainers\": \"name\": \"$maintainer_name\",\n   \"donate_url\": \"https://paypal.me/travarilo\",\n   \"website_url\": \"https://bananadroid.com\",\n   \"forum_url\": \"https://t.me/bananadroid\"\n}" > $output
+    echo -e "{\n   \"error\": "false",\n   \"version\": \"13\",\n   \"url\": \"https://sourceforge.net/projects/bananadroid/files/$device_name/$file_name\",\n   \"github_releases_url\": \"https://sourceforge.net/projects/bananadroid/files/$device_name/$file_name\",\n   \"datetime\": $datetime,\n   \"filehash\": \"$md5\",\n   \"filename\": \"$file_name\",\n   \"id\": \"$id\",\n   \"size\": $file_size,\n   \"maintainers\": [\n        {\n           \"github_username\": \"$maintainer_name\",\n           \"name\": \"$maintainer_name\"\n        }\n   ],\n   \"donate_url\": \"https://paypal.me/travarilo\",\n   \"website_url\": \"https://bananadroid.com\",\n   \"forum_url\": \"https://t.me/bananadroid\"\n}" > $output
     rm -rf "./$DEVICE.json"
     cp $output "./$DEVICE.json"
   fi
