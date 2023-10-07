7d689da9b40b4f4ee3c36a6b9da7d9a66acc834c
maxwen <max.weninger@gmail.com>
2023-10-07 18:51:12 +0200

    fixup! make soong variables follow json syntax
    TODO the subst does noting now so should be easier to write
    
    wrong: CROSS_COMPILE=\\"/data4...
    correct: CROSS_COMPILE=\"/data4...
    
    Change-Id: I5080aef7feb2ec7429cb9ba1f4baccefa61a3cd3

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -21,7 +21,7 @@ SOONG_CONFIG_bananaVarsPlugin :=
 
 define addVar
   SOONG_CONFIG_bananaVarsPlugin += $(1)
-  SOONG_CONFIG_bananaVarsPlugin_$(1) := $$(subst ",",$$($1))
+  SOONG_CONFIG_bananaVarsPlugin_$(1) := $($1)
 endef
 
 $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
