38f2fc0ada5a583a527d8ef50f3d7f75399187d8
LuK1337 <priv.luk@gmail.com>
2024-02-14 09:22:34 +0100

    roomservice: Remove python2 imports
    Change-Id: Id65a4c139f14106632587634c42ef9941b55d0c1

--- build/tools/roomservice.py
+++ build/tools/roomservice.py
@@ -21,23 +21,11 @@ import json
 import netrc
 import os
 import sys
+import urllib.error
+import urllib.parse
+import urllib.request
 
 from xml.etree import ElementTree
-
-try:
-    # For python3
-    import urllib.error
-    import urllib.parse
-    import urllib.request
-except ImportError:
-    # For python2
-    import imp
-    import urllib2
-    import urlparse
-    urllib = imp.new_module('urllib')
-    urllib.error = urllib2
-    urllib.parse = urlparse
-    urllib.request = urllib2
 
 DEBUG = False
 
